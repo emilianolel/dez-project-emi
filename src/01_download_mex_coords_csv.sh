f3b24433fc5e604133c643e907fbc96507268488
emilianolel <eherrera1331@gmail.com>
2024-03-21 22:13:33 -0600

    code to ingest coords file

---
+++ src/01_download_mex_coords_csv.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+TEMP_DIRECTORY=../tmp/mex_coords/
+
+DOWNLOAD_LINK='https://raw.githubusercontent.com/eduardoarandah/coordenadas-estados-municipios-localidades-de-mexico-json/master/data.csv'
+
+CSV_FILE_NAME=mex_coords.csv
+
+CSV_DIR_NAME=mex_coords/
+
+BUCKET_PATH=gs://landing_bucket_dez/
+
+echo
+echo ' EXECUTING FILE: ' $0
+
+echo
+echo 'CREATING TEMP DIR'
+mkdir -p $TEMP_DIRECTORY
+
+echo
+echo 'DOWNLOADING IDM CSV FILE'
+curl -o $TEMP_DIRECTORY$CSV_FILE_NAME -LJ $DOWNLOAD_LINK
+
+echo
+echo 'MOVING IDM CSV FILE TO GCS'
+gsutil -m cp $TEMP_DIRECTORY$CSV_FILE_NAME $BUCKET_PATH$CSV_DIR_NAME$CSV_FILE_NAME
+
+echo
+echo 'DELETING TEMP DIRECTORY'
+rm -rf $TEMP_DIRECTORY
+
+echo
+echo 'DONE!'
