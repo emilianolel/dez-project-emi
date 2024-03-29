ed5cc2c63d4b8689fdb1406fba42c33cf7097c88
emilianolel <eherrera1331@gmail.com>
2024-03-29 17:55:43 -0600

    terraform advanced tweaks

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,10 +13,14 @@ provider "google" {
   region      = var.region
 }
 
-resource "google_compute_instance" "dez-project-emil-vm" {
+resource "google_compute_address" "static-ip-address" {
+  name = var.static_ip_address_name
+}
+
+resource "google_compute_instance" "terraform-test-intance" {
   boot_disk {
     auto_delete = true
-    device_name = "dez-project-emil-vm"
+    device_name = var.compute_instance_name 
 
     initialize_params {
       image = "projects/ubuntu-os-cloud/global/images/ubuntu-2004-focal-v20240307b"
@@ -35,17 +39,18 @@ resource "google_compute_instance" "dez-project-emil-vm" {
     goog-ec-src = "vm_add-tf"
   }
 
-  machine_type = "e2-standard-8"
-  name         = "dez-project-emil-vm"
+  machine_type = "e2-standard-4"
+  name         = var.compute_instance_name 
 
   network_interface {
+    network = "default"
     access_config {
-      network_tier = "PREMIUM"
+      nat_ip = google_compute_address.static-ip-address.address
     }
+  }
 
-    queue_count = 0
-    stack_type  = "IPV4_ONLY"
-    subnetwork  = "projects/dez-workspace-emil/regions/us-west4/subnetworks/default"
+  metadata = {
+    ssh-keys = "${var.ssh_username}:${file(var.ssh_pub_key)}"
   }
 
   scheduling {
@@ -56,7 +61,7 @@ resource "google_compute_instance" "dez-project-emil-vm" {
   }
 
   service_account {
-    email  = "dez-project-emil@dez-workspace-emil.iam.gserviceaccount.com"
+    email  = var.service_account_email # set variable
     scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 
@@ -66,35 +71,99 @@ resource "google_compute_instance" "dez-project-emil-vm" {
     enable_vtpm                 = true
   }
 
-  zone = "us-west4-b"
+  zone = var.zone
 }
 
-resource "random_string" "bucket_name" {
-  length           = 32
-  special          = false
-  lower            = true
+resource "random_id" "bucket_prefix" {
+  byte_length = 8
 }
 
 resource "google_storage_bucket" "landing_bucket" {
-  name          = sha1(random_string.bucket_name.result)
-  location      = var.location
+  name          = format("%s-%s", var.landing_bucket_name, "${random_id.bucket_prefix.hex}")
+  location      = "US"
   force_destroy = true
+}
 
-  lifecycle_rule {
-    condition {
-      age = 3
-    }
-    action {
-      type = "Delete"
-    }
-  }
-  lifecycle_rule {
-    condition {
-      age = 1
-    }
-    action {
-      type = "AbortIncompleteMultipartUpload"
-    }
+resource "google_storage_bucket" "parquet_bucket" {
+  name          = format("%s-%s", var.parquet_bucket_name, "${random_id.bucket_prefix.hex}")
+  location      = "US"
+  force_destroy = true
+}
+
+resource "google_storage_bucket" "dataproc_bucket" {
+  name          = format("%s-%s", var.dataproc_bucket_name, "${random_id.bucket_prefix.hex}")
+  location      = "US"
+  force_destroy = true
+}
+
+resource "google_bigquery_dataset" "raw_geo_mx" {
+  dataset_id                  = var.geo_dataset_name
+  access {
+    role          = "OWNER"
+    user_by_email = var.service_account_email
   }
 }
 
+resource "google_bigquery_dataset" "raw_dez_crimes" {
+  dataset_id                  = var.crimes_dataset_name
+  access {
+    role          = "OWNER"
+    user_by_email = var.service_account_email
+  }
+}
+resource "google_bigquery_dataset" "dez_project_mx_crimes" {
+  dataset_id                  = var.mx_crimes_dataset_name
+  access {
+    role          = "OWNER"
+    user_by_email = var.service_account_email
+  }
+}
+
+resource "google_dataproc_cluster" "dataproc_cluster" {
+  name     = var.dataproc_cluster_name
+  region   = "us-central1"
+  graceful_decommission_timeout = "120s"
+  
+  labels = {
+    env = "dev"
+    foo = "bar"
+  }
+
+  cluster_config {
+    staging_bucket = google_storage_bucket.dataproc_bucket.name
+
+    master_config {
+      num_instances = 1
+      machine_type  = "e2-standard-2"
+      disk_config {
+        boot_disk_type    = "pd-standard"
+        boot_disk_size_gb = 100
+      }
+    }
+
+    worker_config {
+      num_instances    = 2
+      machine_type     = "e2-standard-4"
+      disk_config {
+        boot_disk_size_gb = 30
+        num_local_ssds    = 0
+      }
+    }
+
+    preemptible_worker_config {
+      num_instances = 0
+    }
+
+    # Override or set some custom properties
+    software_config {
+      image_version = "2.1.43-ubuntu20"
+      override_properties = {
+        "dataproc:dataproc.allow.zero.workers" = "true"
+      }
+    }
+
+  }
+  depends_on = [
+        google_storage_bucket.dataproc_bucket
+    ]
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,6 +6,11 @@ variable "my_creds" {
 
 variable "region" {
   description = "Region"
+  default     = "us-west4"
+}
+
+variable "zone" {
+  description = "Zone"
   default     = "us-west4-b"
 }
 
@@ -19,3 +24,62 @@ variable "location" {
   default     = "US"
 }
 
+variable "static_ip_address_name" {
+    description = "Static IP Address Name"
+    default = "my-static-ip-address"
+}
+
+variable "service_account_email" {
+    description = "Service Account Email"
+    default = "dez-project-emil@dez-workspace-emil.iam.gserviceaccount.com"
+}
+
+variable "compute_instance_name" {
+    description = "Compute Instance Name"
+    default = "terraform-test-instance"
+}
+
+variable "ssh_username" {
+    description = "SSH Username"
+    default = "emilel"
+}
+
+variable "ssh_pub_key" {
+    description = "Path to pub key"
+    default = "../.ssh/gcp.pub"
+}
+
+variable "landing_bucket_name" {
+    description = "Landing Bucket Name"
+    default = "landing-bucket"
+}
+
+variable "parquet_bucket_name" {
+    description = "Landing Bucket Name"
+    default = "parquet-bucket"
+}
+
+variable "dataproc_bucket_name" {
+    description = "Dataproc Bucket Name"
+    default = "dataproc-staging-bucket-dez-test"
+}
+
+variable "geo_dataset_name" {
+    description = "Geographical Dataset Name"
+    default = "raw_geo_mx_test"
+}
+
+variable "crimes_dataset_name" {
+    description = "Crimes Dataset Name"
+    default = "raw_dez_crimes_test"
+}
+
+variable "mx_crimes_dataset_name" {
+    description = "Fact MX Dataset Name"
+    default = "dez_project_mx_crimes_test"
+}
+
+variable "dataproc_cluster_name" {
+    description = "Dataproc Cluster Name"
+    default = "dez-project-cluster"
+}
